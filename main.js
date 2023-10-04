770cc8f490368a21e86aabf2251ff7ea00ab12ca
Mark Mwangi <mwangimark57@gmail.com>
2023-10-04 14:30:48 +0300

    Completedd Assignment

--- main.js
+++ main.js
@@ -4,7 +4,50 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const emptyHearts = document.querySelectorAll('.like-glyph');
+  
+  // Add click event listeners to empty hearts
+  emptyHearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+      likeUnlikePost(heart);
+    });
+  });
 
+  // Add click event listeners to full hearts
+  const fullHearts = document.querySelectorAll('.activated-heart');
+  fullHearts.forEach((heart) => {
+    heart.addEventListener('click', () => {
+      likeUnlikePost(heart);
+    });
+  });
+});
+
+function likeUnlikePost(heart) {
+  mimicServerCall()
+    .then(() => {
+      if (heart.classList.contains('activated-heart')) {
+        // Unlike: Change the heart back to empty
+        heart.classList.remove('activated-heart');
+        heart.textContent = EMPTY_HEART;
+      } else {
+        // Like: Change the heart to full
+        heart.classList.add('activated-heart');
+        heart.textContent = FULL_HEART;
+      }
+    })
+    .catch((error) => {
+      const errorModal = document.getElementById('modal');
+      const errorMessage = document.getElementById('modal-message');
+      errorMessage.textContent = error;
+      errorModal.classList.remove('hidden');
+
+      // Hide the error modal after 3 seconds
+      setTimeout(() => {
+        errorModal.classList.add('hidden');
+      }, 3000);
+    });
+}
 
 
 //------------------------------------------------------------------------------
